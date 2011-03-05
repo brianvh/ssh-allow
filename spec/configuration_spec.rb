adafdeb7d08c20a6df043e6628256b23313ba496
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-04 23:07:32 -0500

    Verified that we can properly add a rule and reset the rules array.

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -15,4 +15,30 @@ describe SSH::Allow::Configuration do
       @config.rules.should be_empty
     end
   end
+
+  context "after adding a valid rule" do
+    before(:each) do
+      @rule = mock(:valid)
+      @config.should_receive(:parse_command).once.and_return(@rule)
+      @config.allow(:rule)
+    end
+
+    it "has 1 rule" do
+      @config.rules.should have(1).items
+    end
+
+    it "contains the correct rule in the rules array" do
+      @config.rules.should == [@rule]
+    end
+
+    context "after resetting" do
+      before(:each) do
+        @config.reset!
+      end
+
+      it "has 0 rules" do
+        @config.rules.should have(0).items
+      end
+    end
+  end
 end
