a1f8bedd2ca5bdc91c3a0255c44c448beaab0f8d
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-04 22:03:42 -0500

    Added show_stderr CLI spec helper method.

--- spec/support/cli_helpers.rb
+++ spec/support/cli_helpers.rb
@@ -5,6 +5,11 @@ module CLIHelpers
     @announce_stdout = true
   end
 
+  def show_stderr
+    @puts = true
+    @announce_stderr = true
+  end
+
   def ssh_command(cmd)
     ENV['SSH_REMOTE_COMMAND'] = cmd
   end
