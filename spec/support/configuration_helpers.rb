16d2a9add5180067062e1af022e1002e03bf2f28
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-05 01:35:09 -0500

    Added ConfigurationHelpers#sample_rules helper method.
     * Provides a reliable block of text that will parse into 2 Rule objects.
     * Almost always used in conjunction with mocking config file reading as well
       as mocking the Rule objects that are handed to the Configuration instance.

--- spec/support/configuration_helpers.rb
+++ spec/support/configuration_helpers.rb
@@ -4,4 +4,15 @@ module ConfigurationHelpers
     rule.should_receive(:valid?).once.and_return(valid)
     rule
   end
+
+  def sample_rules
+    %(
+    allow!('foo') do
+      puts 'foo'
+    end
+    allow!('bar') do
+      puts 'bar'
+    end
+    ).gsub(/^ {4}/, '')
+  end
 end
