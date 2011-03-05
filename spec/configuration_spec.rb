cee0adfa765d386693898777f8a03fdca7c4a616
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-04 23:49:41 -0500

    Cleaned up the contexts and some examples in configuration_spec.
     * Now using the mock_rule helper method when we need a rule object.

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
-require 'ssh/allow/configuration'
 
 describe SSH::Allow::Configuration do
   before(:each) do
     @config = SSH::Allow::Configuration.new
   end
 
-  context "when instantiated" do
+  context "#new" do
     it "is valid" do
       @config.should be_valid
     end
@@ -16,43 +15,41 @@ describe SSH::Allow::Configuration do
     end
   end
 
-  context "after adding a valid rule" do
+  context "#allow -- on a valid rule" do
     before(:each) do
-      @rule = mock(:valid)
-      @rule.should_receive(:valid?).once.and_return(true)
+      @rule = mock_rule(:invalid)
       @config.should_receive(:parse_command).once.and_return(@rule)
       @config.allow(:rule)
     end
 
-    it "has 1 rule" do
-      @config.rules.should have(1).items
-    end
-
-    it "contains the correct rule in the rules array" do
+    it "adds the rule" do
       @config.rules.should == [@rule]
     end
 
-    context "after resetting" do
+    context "#reset!" do
       before(:each) do
         @config.reset!
       end
 
-      it "has 0 rules" do
-        @config.rules.should have(0).items
+      it "clears all rules" do
+        @config.rules.should be_empty
       end
     end
   end
 
-  context "after adding an invalid rule" do
+  context "#allow -- on an invalid rule" do
     before(:each) do
-      @rule = mock(:invalid)
-      @rule.should_receive(:valid?).once.and_return(false)
-      @config.should_receive(:parse_command).once.and_return(@rule)
-      @config.allow(:rule)
+      rule = mock_rule(:invalid, false)
+      @config.should_receive(:parse_command).once.and_return(rule)
+      @allow = @config.allow(:rule)
     end
 
-    it "has 0 rules" do
-      @config.rules.should have(0).items
+    it "returns false" do
+      @allow.should == false
+    end
+
+    it "does not add the rule" do
+      @config.rules.should be_empty
     end
   end
 end
