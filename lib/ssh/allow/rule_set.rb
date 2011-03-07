07e0e3be07bc79dda61f9894b203bd4799f033d1
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-07 14:12:19 -0500

    Changed Configuration class to RuleSet.
     * Renamed many of the private methods.
     * Removed the @error instance variable and the valid? method.
     * Removed the reset! method, which has no real purpose.

--- lib/ssh/allow/rule_set.rb
+++ lib/ssh/allow/rule_set.rb
@@ -2,51 +2,42 @@ require 'singleton'
 
 module SSH::Allow
 
-  class Configuration
-    attr_reader :rules, :error
+  class RuleSet
+    attr_reader :rules
 
     def initialize
       @rules = []
-      @error = nil
-    end
-
-    def valid?
-      error.nil?
     end
 
     def allow(cmd, &block)
-      add_rule parse_command(cmd, block)
+      push get_rule(cmd, block)
     end
 
     def allow!(cmd, &block)
-      rule = parse_command(cmd, block)
-      add_rule(rule) or raise(%(Invalid rule: "#{cmd}"))
+      rule = get_rule(cmd, block)
+      push(rule) or raise(%(Invalid rule: "#{cmd}"))
     end
 
-    def read(path_to_config)
-      self.instance_eval(read_config(path_to_config))
-    end
-
-    def reset!
-      @rules.clear
+    def read(path_to_rules)
+      self.instance_eval(read_rules(path_to_rules))
     end
 
     private
 
-      def add_rule(rule)
+      def push(rule)
         rule.valid? ? @rules.push(rule) : false
       end
 
-      def parse_command(cmd, &block)
+      def get_rule(cmd, &block)
         SSH::Allow::Rule.parse(cmd, block)
       end
 
-      def read_config(path_to_config)
-        IO.read(path_to_config)
+      def read_rules(path_to_rules)
+        IO.read(path_to_rules)
       end
   end
 
-  class Configuration::Single < Configuration
+  class RuleSet::Single < RuleSet
     include Singleton
   end
 
