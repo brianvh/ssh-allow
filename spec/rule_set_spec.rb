b34857777629a44ee68940e8dd4b09509da7752b
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-07 14:13:25 -0500

    Modified Configuration spec to RuleSet.
     * All specs pass.

--- spec/rule_set_spec.rb
+++ spec/rule_set_spec.rb
@@ -1,49 +1,35 @@
 require 'spec_helper'
 
-describe SSH::Allow::Configuration do
+describe SSH::Allow::RuleSet do
   before(:each) do
-    @config = SSH::Allow::Configuration.new
+    @rule_set = SSH::Allow::RuleSet.new
   end
 
   context "#new" do
-    it "is valid" do
-      @config.should be_valid
-    end
-
     it "has an empty rules array" do
-      @config.rules.should be_empty
+      @rule_set.rules.should be_empty
     end
   end
 
   context "with a valid rule" do
     before(:each) do
       @rule = mock_rule(:invalid)
-      @config.should_receive(:parse_command).once.and_return(@rule)
+      @rule_set.should_receive(:get_rule).once.and_return(@rule)
     end
 
     context "#allow" do
       before(:each) do
-        @config.allow(:rule)
+        @rule_set.allow(:rule)
       end
 
       it "adds the rule" do
-        @config.rules.should == [@rule]
-      end
-
-      context "#reset!" do
-        before(:each) do
-          @config.reset!
-        end
-
-        it "clears all rules" do
-          @config.rules.should be_empty
-        end
+        @rule_set.rules.should == [@rule]
       end
     end
 
     context "#allow!" do
       it "does not raise an error" do
-        lambda { @config.allow!(:rule) }.should_not raise_error
+        lambda { @rule_set.allow!(:rule) }.should_not raise_error
       end
     end
   end
@@ -51,12 +37,12 @@ describe SSH::Allow::Configuration do
   context "with an invalid rule" do
     before(:each) do
       rule = mock_rule(:invalid, false)
-      @config.should_receive(:parse_command).once.and_return(rule)
+      @rule_set.should_receive(:get_rule).once.and_return(rule)
     end
 
     context "#allow" do
       before(:each) do
-        @allow = @config.allow(:rule)
+        @allow = @rule_set.allow(:rule)
       end
 
       it "returns false" do
@@ -64,13 +50,13 @@ describe SSH::Allow::Configuration do
       end
 
       it "does not add the rule" do
-        @config.rules.should be_empty
+        @rule_set.rules.should be_empty
       end
     end
 
     context "#allow!" do
       it "raises an error" do
-        lambda { @config.allow!(:rule) }.should raise_error(/Invalid rule: "rule"/)
+        lambda { @rule_set.allow!(:rule) }.should raise_error(/Invalid rule: "rule"/)
       end
     end
   end
@@ -79,17 +65,17 @@ describe SSH::Allow::Configuration do
     before(:each) do
       @rule1 = mock_rule(:foo)
       @rule2 = mock_rule(:bar)
-      @config.should_receive(:parse_command).twice.and_return(@rule1, @rule2)
-      @config.should_receive(:read_config).once.and_return(sample_rules)
+      @rule_set.should_receive(:get_rule).twice.and_return(@rule1, @rule2)
+      @rule_set.should_receive(:read_rules).once.and_return(sample_rules)
     end
 
     context "#read" do
       before(:each) do
-        @config.read('/my/fake/rules')
+        @rule_set.read('/my/fake/rules')
       end
 
       it "adds 2 rules" do
-        @config.rules.should == [@rule1, @rule2]
+        @rule_set.rules.should == [@rule1, @rule2]
       end
     end
   end
@@ -98,13 +84,13 @@ describe SSH::Allow::Configuration do
     before(:each) do
       @rule1 = mock_rule(:foo)
       @rule2 = mock_rule(:bar, false)
-      @config.should_receive(:parse_command).twice.and_return(@rule1, @rule2)
-      @config.should_receive(:read_config).once.and_return(sample_rules)
+      @rule_set.should_receive(:get_rule).twice.and_return(@rule1, @rule2)
+      @rule_set.should_receive(:read_rules).once.and_return(sample_rules)
     end
 
     context "#read" do
       it "raises an error on the second rule" do
-        lambda { @config.read('/my/fake/rules') }.should raise_error(/Invalid rule: "bar"/)
+        lambda { @rule_set.read('/my/fake/rules') }.should raise_error(/Invalid rule: "bar"/)
       end
     end
   end
