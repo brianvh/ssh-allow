3a95f0809f452d3f6068e6c92cbc6b02111feb86
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-07 14:59:27 -0500

    Added the basic Rule class.
     * Require the rule class file from the main library file.

---
+++ lib/ssh/allow/rule.rb
@@ -0,0 +1,14 @@
+module SSH::Allow
+  class Rule
+    def initialize
+      @command = []
+      @options = []
+      @args = []
+    end
+
+    def valid?
+      !(@command.empty? && @options.empty? && @args.empty?)
+    end
+
+  end
+end
