29f7f7e2490155f2ad5aa1b76721ab665d2c9415
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-07 14:10:38 -0500

    Main library file now requires the rule_set file.

--- lib/ssh/allow.rb
+++ lib/ssh/allow.rb
@@ -1,12 +1,12 @@
 require 'ssh/allow/cli'
-require 'ssh/allow/configuration'
+require 'ssh/allow/rule_set'
 
 module SSH
   module Allow
 
     # module wrapper method for accessing the Singleton Configuration instance
-    def self.configure
-      SSH::Allow::Configuration::Single.instance
+    def self.rules
+      SSH::Allow::RuleSet::Single.instance
     end
 
     # module wrapper method for parsing and verifying the passed in command string
