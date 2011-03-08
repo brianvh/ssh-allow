3064ec4751ab3b0d2dc37507e2c7febc2c888ea4
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-08 16:08:26 -0500

    Added additional examples to rule_spec.
     * Handle creating a Rule instance with a single command: without a block,
       with a valid block and with an invalid block.

--- spec/rule_spec.rb
+++ spec/rule_spec.rb
@@ -2,15 +2,54 @@ require 'spec_helper'
 
 describe SSH::Allow::Rule do
 
-  context "creating an Allow rule" do
-    context "with a single command string" do
+  context "creating an Allow rule, with a single command" do
+    before(:each) do
+      @cmd = "ls"
+    end
+
+    context "without a block" do
       before(:each) do
-        @cmd = "ls"
         @rule = SSH::Allow::Rule.allow(@cmd)
       end
 
-      it "returns an Allow rule" do
-        @rule.should be_instance_of(SSH::Allow::Rule::Allow)
+      context "the rule options" do
+        it "returns :none" do
+          @rule.options.should == [:none]
+        end
+      end
+    end
+
+    context "with a valid block" do
+      before(:each) do
+        @rule = SSH::Allow::Rule.allow(@cmd) do
+          opts "-ld"
+          args "/foo/bar/.*"
+        end
+      end
+
+      context "the rule options" do
+        it "returns \"-ld\"" do
+          @rule.options.should == ["-ld"]
+        end
+      end
+
+      context "the rule arguments" do
+        it "returns \"/foo/bar/.*\"" do
+          @rule.arguments.should == ["/foo/bar/.*"]
+        end
+      end
+    end
+
+    context "with an invalid block" do
+      before(:each) do
+        @rule = SSH::Allow::Rule.allow(@cmd) do
+          opts "-ld"
+          foo "bar"
+        end
+      end
+
+      it "returns false" do
+        @rule.should be_false
       end
     end
   end
