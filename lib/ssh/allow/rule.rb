29cd6ac0920f25ea7dc8e78350f7e44c152030d5
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-08 16:07:23 -0500

    Added block instantiation for Rule objects.
     * Also modified Rule.create to return false if evaluating the block fails.

--- lib/ssh/allow/rule.rb
+++ lib/ssh/allow/rule.rb
@@ -1,13 +1,27 @@
 module SSH ; module Allow
 
   class Rule
-    attr_reader :command
+    attr_reader :command, :options, :arguments
 
     def initialize(cmds)
       @command = [cmds].flatten
       @options = [:none]
       @arguments = [:none]
     end
+
+    def opts(opt_text)
+      push(:options, opt_text)
+    end
+
+    def args(arg_text)
+      push(:arguments, arg_text)
+    end
+
+    def push(attrib, value)
+      send(attrib).clear if send(attrib) == [:none]
+      send(attrib).push(value)
+    end
+    private :push
 
     class << self
       def allow(*cmds, &block)
@@ -19,8 +33,12 @@ module SSH ; module Allow
       end
 
       def create(rule, &block)
-        rule.instance_eval(&block) if block_given?
-        rule
+        begin
+          rule.instance_eval(&block) if block_given?
+          rule
+        rescue Exception => e
+          false
+        end
       end
     end
 
