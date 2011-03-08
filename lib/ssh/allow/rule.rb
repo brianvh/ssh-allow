72b0e32b338ca784c54eee5e3599ee1da421ab87
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-08 15:30:52 -0500

    Rule constructor class methods return Allow or Deny objects.
     * Allow and Deny class definitions are part of the Rule class, since they exist
       only within the confines of being Rule instances.

--- lib/ssh/allow/rule.rb
+++ lib/ssh/allow/rule.rb
@@ -1,14 +1,36 @@
-module SSH::Allow
+module SSH ; module Allow
+
   class Rule
-    def initialize
-      @command = []
-      @options = []
-      @args = []
+    attr_reader :command
+
+    def initialize(cmds)
+      @command = [cmds].flatten
+      @options = [:none]
+      @arguments = [:none]
     end
 
-    def valid?
-      !(@command.empty? || @options.empty? || @args.empty?)
+    class << self
+      def allow(*cmds, &block)
+        create(Rule::Allow.new(cmds.flatten), &block)
+      end
+
+      def deny(*cmds, &block)
+        create(Rule::Deny.new(cmds.flatten), &block)
+      end
+
+      def create(rule, &block)
+        rule.instance_eval(&block) if block_given?
+        rule
+      end
     end
 
+    class Allow < Rule
+      
+    end
+
+    class Deny < Rule
+      
+    end
   end
-end
+
+end ; end
