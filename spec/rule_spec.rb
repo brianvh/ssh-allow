a897bf43087a3e59bb773fd0fa7ca7560c3d07d9
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-08 15:32:18 -0500

    Modified rule_spec to verify the construction of the Rule sub-classes.

--- spec/rule_spec.rb
+++ spec/rule_spec.rb
@@ -1,13 +1,30 @@
 require 'spec_helper'
 
 describe SSH::Allow::Rule do
-  context "when initially created" do
-    before(:each) do
-      @rule = SSH::Allow::Rule.new
-    end
 
-    it "is invalid" do
-      @rule.should_not be_valid
+  context "creating an Allow rule" do
+    context "with a single command string" do
+      before(:each) do
+        @cmd = "ls"
+        @rule = SSH::Allow::Rule.allow(@cmd)
+      end
+
+      it "returns an Allow rule" do
+        @rule.should be_instance_of(SSH::Allow::Rule::Allow)
+      end
+    end
+  end
+
+  context "creating a Deny rule" do
+    context "with a single command string" do
+      before(:each) do
+        @cmd = "ls"
+        @rule = SSH::Allow::Rule.deny(@cmd)
+      end
+
+      it "returns a Deny rule" do
+        @rule.should be_instance_of(SSH::Allow::Rule::Deny)
+      end
     end
   end
 end
