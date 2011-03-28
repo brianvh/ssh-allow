70f98341626a1706abbd9b6a4153928215c4285b
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-28 09:15:39 -0400

    Added the stub for the Command class.
     * Set up the requires for the treetop grammar.
     * Required the Command class in the main module file.

--- lib/ssh/allow.rb
+++ lib/ssh/allow.rb
@@ -1,6 +1,7 @@
 require 'ssh/allow/cli'
 require 'ssh/allow/rule_set'
 require 'ssh/allow/rule'
+require 'ssh/allow/command'
 
 module SSH
   module Allow
@@ -12,7 +13,7 @@ module SSH
 
     # module wrapper method for parsing and verifying the passed in command string
     def self.command(cmd_str)
-      SSH::Allow::Command.guard(cmd_str)
+      @@command ||= SSH::Allow::Command.guard(cmd_str)
     end
 
   end
---
+++ lib/ssh/allow/command.rb
@@ -0,0 +1,14 @@
+$:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__))
+
+require 'polyglot'
+require 'treetop'
+require 'command_line'
+
+module SSH ; module Allow
+  class Command
+    def initialize(args)
+      
+    end
+
+  end
+end ; end
