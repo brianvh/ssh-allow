3363f1a326400f5d6eec916d0dcec760a004d3fc
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-28 17:36:03 -0400

    Moved the module methods into the CLI class.
     * Those methods are only called from the CLI class, so there's no reason for
       them to be defined, elsewhere.
     * Also, no longer using the Single subclass of RuleSet. Need to modify the
       class to remove the Singleton module.

--- lib/ssh/allow.rb
+++ lib/ssh/allow.rb
@@ -2,19 +2,3 @@ require 'ssh/allow/cli'
 require 'ssh/allow/rule_set'
 require 'ssh/allow/rule'
 require 'ssh/allow/command'
-
-module SSH
-  module Allow
-
-    # module wrapper method for accessing the Singleton Configuration instance
-    def self.rules
-      SSH::Allow::RuleSet::Single.instance
-    end
-
-    # module wrapper method for parsing and verifying the passed in command string
-    def self.command(cmd_str)
-      @@command ||= SSH::Allow::Command.guard(cmd_str)
-    end
-
-  end
-end
