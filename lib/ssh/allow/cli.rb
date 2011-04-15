3ff4c757a305538f530c247744911b1da9fb8e42
Brian V. Hughes <brianvh@dartmouth.edu>
2011-04-15 13:41:33 -0400

    Cleaning up the CLI methods.
     * No need to pass message to fail. Need to add a to_s method to Command for
       returning the text of the SSH_REMOTE_COMMAND.

--- lib/ssh/allow/cli.rb
+++ lib/ssh/allow/cli.rb
@@ -11,8 +11,8 @@ module SSH::Allow
                   :banner => "Echo the SSH_REMOTE_COMMAND."
     def guard
       rule_set.read(options[:rules])
-      puts ssh_cmd if options[:echo]
-      command.allow?(rule_set.rules) ? command.run : fail(command.fail)
+      puts command if options[:echo]
+      command.allow?(rule_set.rules) ? command.run : fail
     end
 
     private
@@ -25,8 +25,8 @@ module SSH::Allow
       @command ||= SSH::Allow::Command.new(ENV['SSH_REMOTE_COMMAND'])
     end
 
-    def fail(msg='ssh-allow: Something went wrong.')
-      raise Thor::Error, msg
+    def fail
+      raise "Remote Command Not Allowed: #{command}"
     end
 
   end
